a6dc04af760499bb0807c678de1852cb74fd98fa
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2024-08-22 18:08:25 -0500

    added more JavaScript to website with functions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(let x=0;x<topics.length;x++){
-    console.log("Let's learn " + topics[x] + "!")
+function listTopics(){
+    for(let x=0;x<topics.length;x++){
+        console.log(topics[x])
+    }
+
 }
+function helloWorld(){
+    console.log("Hello, World!")
+}
+function selectTopic(topic) {
+    if (topic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+}
+helloWorld();
+console.log("Here are a list of topics we covered in the Prework Section:")
+listTopics();
+console.log("Which topic should we study first?")
+selectTopic(randomTopic);
